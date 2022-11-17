a8a70324cf085e86ff1923d5b8a5fd4e9e34b04d
Rui Cong Tham <e0726692@u.nus.edu>
2022-11-17 10:02:47 +0800

    Change float output to 2dp
    Change-Id: I85f9c97ff2a5260b3e2cd10419183fdaefe6e142

--- src/main/java/altcompetition/OrderBooks.java
+++ src/main/java/altcompetition/OrderBooks.java
@@ -114,7 +114,7 @@ public class OrderBooks extends CompetitionTest {
             }
         }
 
-        return Float.toString(totalSpent);
+        return String.format("%.2f", totalSpent);
     }
 
     public static void main(String[] args) {
@@ -127,5 +127,12 @@ public class OrderBooks extends CompetitionTest {
         orders.add("BUY-11-140.00");
         
         System.out.println(new OrderBooks().processOrderBooks(orders));
+
+        List<String> orders2 = new ArrayList<String>();
+        orders2.add("BUY-11-10.00");
+        orders2.add("SELL-10-9.00");
+        orders2.add("SELL-1-1");
+        
+        System.out.println(new OrderBooks().processOrderBooks(orders2));
     }
 }
